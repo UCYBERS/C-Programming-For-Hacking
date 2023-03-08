7ab534887f379d6d9886a7a0b74d16b343975049
ucybers <90274068+UCYBERS@users.noreply.github.com>
2023-03-08 20:22:50 +0300

    Create ep3_DateType.c

---
+++ Course exercises/ep3_DateType.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+   float a =10.72643;
+    float b=15.7;
+    char lt ='D';
+  char name[20] = "Dark UCYBERS"; 
+    printf("Hello world!\n");
+    printf("the value of a is %.2f\nthe value of b is %.1f\n",a,b);
+    printf("%c\n",lt);
+  printf("My name is %s\n",name);
+  printf("%c\n",name[0]);
+  printf("%c\n",name[1]);
+  printf("%c\n",name[2]);
+  printf("%c\n",name[3]);
+  printf("%c\n",name[4]);
+  printf("%c\n",name[5]);
+  printf("%c\n",name[6]);
+  printf("%c\n",name[7]);
+  printf("%c\n",name[8]);
+  printf("%c\n",name[9]);
+  printf("%c\n",name[10]);
+  printf("%c\n",name[11]);
+    return 0;
