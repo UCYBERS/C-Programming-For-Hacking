2e8c1b53bdf8f2d93be202ebf4f155e6f287c79d
ucybers <90274068+UCYBERS@users.noreply.github.com>
2023-10-31 17:36:39 +0300

    Episode17.2.c

--- Course exercises/Episode17.2.c
+++ Course exercises/Episode17.2.c
@@ -1,17 +1,21 @@
 /*
  c program takes a student survey and displays the frequency that people rated 1-5
- and print frequancy histogram as a *
+ and print frequancy histogram as a * and Print the mode, which is the most frequent value
   */
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
 
     int answers[24]={1,2,3,4,5,4,5,2,3,4,5,2,1,3,2,3,4,5,2,3,3};
     int f[6]={0};
+    int maxf=0,mode;
 
     for(int i=1;i<6;i++){
         printf("f[%d] = %d\n",i,f[i]);
     }
-    printf("*******************\n");
+    printf("-------------------------\n");
 
     for(int i=0;i<24;i++){
         f[answers[i]]++;
@@ -28,7 +32,13 @@ int main() {
         }
         printf("\n");
     }
+    for(int i=1;i<=5;i++){
+        if(f[i]>maxf) {maxf=f[i];
+                      mode=i;}
+    }
+    printf("the mode: %d\n",mode);
 
      return 0;
 }
+
 
